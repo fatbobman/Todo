6c3dc2f8fcb2e85617521718a868358061db6a93
Yang Xu <xuyang@me.com>
2022-11-27 08:41:12 +0800

    fix sortType issue

--- ViewLibrary/Sources/ViewLibrary/TaskList.swift
+++ ViewLibrary/Sources/ViewLibrary/TaskList.swift
@@ -60,7 +60,7 @@ public struct TaskListView: View {
                 }
             }
         }
-        .task { @MainActor in
+        .task(id:taskSortType) { @MainActor in
             let (unCompletedRequest, completedRequest) = await getTodoListRequest(taskSource, taskSortType)
             if let unCompletedRequest {
                 $unCompletedTasks = unCompletedRequest
